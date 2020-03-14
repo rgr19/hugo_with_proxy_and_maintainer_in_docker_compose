3e5cd98ba8ad850f53cd3fa1cf00040b0f067f91
rgr19 <onceawaken@outlook.com>
2020-03-14 17:20:48 +0100

    2020_03_14_17_20_48

--- src/main/hugo/src/assets/js/custom.js
+++
@@ -1,4 +0,0 @@
-if ($('#TableOfContents').length > 0) {
-    document.getElementsByClassName("page")[0].style.margin = '0 16rem 0 17rem'
-    document.getElementsByClassName("page")[0].style.padding = '0 1rem 0 4rem'
-}
